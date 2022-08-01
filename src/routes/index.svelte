936a9e6f79fb2fb48466eaffc7253c330da1957e
Glagan <Glagan@users.noreply.github.com>
2022-08-01 15:27:55 +0200

    Add TailwindCSS

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import '../app.css';
+</script>
+
+<h1 class="text-blue-400">Welcome to SvelteKit</h1>
+<p>
+	Visit <a class="text-red-400" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
